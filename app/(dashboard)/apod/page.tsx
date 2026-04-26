6865f40962bdc56f2a1d61b15dd2780c8d102735
Javier Gimenez <jsgimenez@gmail.com>
2026-04-26 15:26:22 -0300

    docs(apod): update what's happens

--- app/(dashboard)/apod/page.tsx
+++ app/(dashboard)/apod/page.tsx
@@ -14,8 +14,8 @@ export default function CounterPage() {
             <h1 className="block text-4xl mb-3">
                 <Link className="text-cyan-800 dark:text-cyan-400 hover:underline" href='https://nextjs.org/docs/pages/building-your-application/rendering/server-side-rendering' target='_blank'>Server Side Rendering</Link>
             </h1>
-            <p className="text-xl">What we've got here is a fetch to <Link className="text-cyan-800 dark:text-cyan-400 hover:underline" href='https://api.nasa.gov/' target='_blank'>NASA APOD API</Link> (<i>aka A Picture of the Day</i>)</p>
-            <p>Data fetching is triggered from the server thru an <Link className="text-cyan-800 dark:text-cyan-400 hover:underline" href='https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Statements/async_function' target='_blank'>async function</Link>, once the response then the data is passed to the client in order to interact with it.</p>
+            <p className="text-xl">What we've got here is a server fetch to <Link className="text-cyan-800 dark:text-cyan-400 hover:underline" href='https://api.nasa.gov/' target='_blank'>NASA APOD API</Link> (<i>aka A Picture of the Day</i>)<br /> thru an <Link className="text-cyan-800 dark:text-cyan-400 hover:underline" href='https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Statements/async_function' target='_blank'>async function</Link>, once the response then the data is passed to the client in order to interact with it.</p >
+            <p className="text-xl">Again, the server is the main responsable to get the data to pass it to the frontend. From user perspective,<br />from user perspective content shown seems served like it would static content. Also, on top of that, IS CACHED! 🥳</p>
 
             <Apod />
             <hr />

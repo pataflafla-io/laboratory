e2221809221553eb6dd02022cdca583554304fc8
Javier Gimenez <jsgimenez@gmail.com>
2026-04-26 20:57:04 -0300

    feat(breadcrumb): add child support

--- components/application/client/breadcrumb-item-path.tsx
+++ components/application/client/breadcrumb-item-path.tsx
@@ -1,19 +1,31 @@
 'use client';
 
-import { BreadcrumbPage, BreadcrumbItem, BreadcrumbSeparator } from '@/components/ui/breadcrumb'
+import { BreadcrumbPage, BreadcrumbItem, BreadcrumbSeparator, BreadcrumbLink } from '@/components/ui/breadcrumb'
 import { usePathname } from 'next/navigation';
+import { Fragment } from 'react/jsx-runtime';
 
 
 export const BreadcrumbItemPath = () => {
-    const currentPath = usePathname()
-    return (
-        currentPath !== '/' &&
-        <>
-            <BreadcrumbSeparator />
-            <BreadcrumbItem>
-                <BreadcrumbPage>{currentPath.slice(1)}</BreadcrumbPage>
-            </BreadcrumbItem>
-        </>
+    const currentPath = usePathname();
+    const paths = currentPath.split('/').slice(1)
 
+    return (
+        <>
+            {
+                currentPath !== '/' &&
+                paths.map((path, i) => (
+                    <Fragment key={path}>
+                        <BreadcrumbSeparator />
+                        <BreadcrumbItem>
+                            {i === paths.length - 1
+                                ? <BreadcrumbPage>{path}</BreadcrumbPage>
+                                : <BreadcrumbLink href={`/${path}`}>{path}</BreadcrumbLink>
+                            }
+                        </BreadcrumbItem>
+                    </Fragment>
+                ))
+            }
+        </>
     )
 }
+

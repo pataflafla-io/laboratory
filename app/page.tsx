4e964904e70447135c81d7daa547efe2d7baa8a0
Javier Gimenez <jsgimenez@gmail.com>
2026-05-06 18:02:39 -0300

    docs(home): updates texts

--- app/page.tsx
+++ app/page.tsx
@@ -3,12 +3,17 @@ import Link from "next/link";
 export default function HomePage() {
   return (
     <div className="flex flex-col flex-1 items-center justify-center font-sans">
-      <main className="flex flex-1 w-full flex-col items-center justify-between py-32 px-16 sm:items-start">
+      <main className="flex flex-1 w-full flex-col items-center justify-between px-16 sm:items-start">
         <section className="flex flex-col w-full">
-          <div className="mx-auto text-center lg:ml-0 lg:text-left">
-            <h1 className="mt-8 max-w-2xl text-balance text-5xl font-medium md:text-6xl lg:mt-16 xl:text-7xl">Learn more 🤓</h1>
+          <div className="mx-auto text-center lg:ml-0 lg:text-left pt-8 lg:pt-16">
+            <h1 className="max-w-2xl text-balance text-5xl font-medium md:text-6xl xl:text-7xl">Learn more 🤓</h1>
             <p className="mt-8 text-pretty text-lg">Space used to share/publish things that i've learned, or i'm currently learning;<br />mostly related to frontend stack and the JavaScript ecosystem.</p>
-            <p className="text-pretty text-lg">Just for the record, the whole lab is built with <Link className="text-cyan-800 dark:text-cyan-400 hover:underline" href="https://www.nextjs.org/" target="_blank">NextJS</Link> framework using <Link className="text-cyan-800 dark:text-cyan-400 hover:underline" href="https://ui.shadcn.com/" target="_blank">Shadcn</Link><br />components as design system and <Link className="text-cyan-800 dark:text-cyan-400 hover:underline" href="https://tailwindcss.com/" target="_blank">TailwindCSS</Link>.</p>
+            <p className="text-pretty text-lg">Just for the record, this sort of laboratory was/is built using <Link href="https://www.nextjs.org/" target="_blank">NextJS</Link>
+              &nbsp;framework<br />alongside <Link href="https://ui.shadcn.com/" target="_blank">Shadcn</Link>
+              &nbsp;components as design system and <Link href="https://tailwindcss.com/" target="_blank">TailwindCSS</Link>.</p>
+            <p className="text-pretty text-lg">Also, the code is deployed at <Link href="https://www.github.com/" target="_blank">github</Link> and hosted/deployed to <Link href="https://www.vercel.com/" target="_blank">Vercel</Link>. </p>
+            <br />
+            <p className="text-pretty text-lg">You can reach me at </p>
           </div>
         </section>
       </main>

f184c985ec6e152b048766f45b844cd9f751fe18
Javier Gimenez <jsgimenez@gmail.com>
2026-05-07 10:47:29 -0300

    docs(home): updates documentation

--- app/page.tsx
+++ app/page.tsx
@@ -6,12 +6,13 @@ export default function HomePage() {
       <main className="flex flex-1 w-full flex-col items-center justify-between px-16 sm:items-start">
         <section className="flex flex-col w-full">
           <div className="mx-auto text-center lg:ml-0 lg:text-left pt-8 lg:pt-16">
-            <h1 className="max-w-2xl text-balance text-5xl font-medium md:text-6xl xl:text-7xl">Learn more 🤓</h1>
-            <p className="mt-8 text-pretty text-lg">Space used to share/publish things that i've learned, or i'm currently learning;<br />mostly related to frontend stack and the JavaScript ecosystem.</p>
+            <h1 className="mb-4 max-w-2xl text-balance text-5xl font-medium md:text-6xl xl:text-7xl">Learn more 🤓</h1>
+            <p className="text-pretty text-lg">Space used to share/publish things that i've learned, or i'm currently learning;<br />mostly related to frontend stack and the JavaScript ecosystem.</p>
+            <p className="mb-3 text-pretty text-lg">It also helps me maintain and improve my english writing skills.</p>
             <p className="text-pretty text-lg">Just for the record, this sort of laboratory was/is built using <Link href="https://www.nextjs.org/" target="_blank">NextJS</Link>
               &nbsp;framework<br />alongside <Link href="https://ui.shadcn.com/" target="_blank">Shadcn</Link>
               &nbsp;components as design system and <Link href="https://tailwindcss.com/" target="_blank">TailwindCSS</Link>.</p>
-            <p className="text-pretty text-lg">Also, the code is deployed at <Link href="https://www.github.com/" target="_blank">github</Link> and hosted/deployed to <Link href="https://www.vercel.com/" target="_blank">Vercel</Link>. </p>
+            <p className="text-pretty text-lg">By the way, the code is hosted at <Link href="https://www.github.com/" target="_blank">github</Link> (private repo at the time i'm writing this)<br />and deployed to <Link href="https://www.vercel.com/" target="_blank">Vercel</Link>. </p>
           </div>
         </section>
       </main>

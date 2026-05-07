4c8eb55860cee10f6be02e19c492f11b24075ccd
Javier Gimenez <jsgimenez@gmail.com>
2026-05-07 17:27:56 -0300

    feat(counter): add fetching to retrieve the counter initial value

--- app/(dashboard)/counter/counterApp.tsx
+++ app/(dashboard)/counter/counterApp.tsx
@@ -1,29 +1,52 @@
 'use client';
 
-import { useState } from 'react'
+import { useEffect } from 'react'
 import { Button } from "@/components/ui/button"
+import { useAppSelector } from '@/store';
+import { useDispatch } from 'react-redux';
+import { initCounterState, addOne, substractOne, resetCount } from '@/store/counter/counterSlice';
+import { CounterResponse } from '@/app/api/counter/CounterResponse';
 
 interface Props {
     initialValue?: number
 }
+
+const getApiCounter = async (): Promise<CounterResponse> => {
+    const data = await fetch('/api/counter').then(response => response.json())
+    return data;
+}
+
 export const Counter = ({ initialValue = 0 }: Props) => {
-    const [value, setvalue] = useState(initialValue);
+    const count = useAppSelector((state) => state.counter.count);
+    const dispatch = useDispatch();
+
+    useEffect(() => {
+        getApiCounter().then(value => {
+            const { count } = value;
+            dispatch(initCounterState(count))
+        })
+    }, [dispatch])
+
+    //const [value, setvalue] = useState(initialValue);
     const handleAddClick = () => {
-        setvalue(value + 1)
+        //setvalue(value + 1)
+        dispatch(addOne())
     }
     const handleResetClick = () => {
-        setvalue(0)
+        //setvalue(0)
+        dispatch(resetCount(0))
     }
     const handleSubstractClick = () => {
-        if (value === 0) return
-        setvalue(value - 1)
+        //if (value === 0) return
+        //setvalue(value - 1)
+        dispatch(substractOne())
     }
 
     return (
         <div className='flex flex-col mt-10 mb-10'>
 
             <h2 className="text-3xl font-semibold tracking-tight">
-                Current value: {value}
+                Current value: {count}
             </h2>
             <div className='flex gap-3 mt-5'>
                 <Button onClick={handleSubstractClick}>-1</Button>

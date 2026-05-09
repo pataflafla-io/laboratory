56457170220b967854fd7905f69aef7403699cbc
Javier Gimenez <jsgimenez@gmail.com>
2026-05-09 17:53:03 -0300

    docs(counter): update documentation

--- app/(dashboard)/counter/page.tsx
+++ app/(dashboard)/counter/page.tsx
@@ -17,13 +17,16 @@ export default async function CounterPage() {
                 Working with <Link href={'https://react.dev/reference/react/useState'} target='_blank'><s>useState</s></Link> <Link href="" target="_blank">Redux toolkit</Link>
             </h1>
             <p className="text-xl"><s>Basic useState example. The initoal value (20) was passed from the server but the component itself (<span className="italic">Counter application</span>) was created from the client side.</s></p>
-            <p className="mt-3 text-xl">Basic Redux example. <s>The initial value (20) was passed from server</s> The initial value (100) was retrieved from an <Link href="/api/counter" target="_blank">endpoint</Link> (<Link href="https://nextjs.org/docs/app/getting-started/route-handlers" target="_blank">rest api</Link>); once retrieved, state is sets with that value in order to dispatch actions. Also, a check flag slice is used in order to keep the value when navigate thru the site.</p>
-            <Counter initialValue={1} />
-            <hr />
+            <p className="mt-3 text-xl">Basic Redux example. <s>The initial value (20) was passed from server</s> The initial value (100) was retrieved from an <Link href="/api/counter" target="_blank">endpoint</Link> (<Link href="https://nextjs.org/docs/app/getting-started/route-handlers" target="_blank">rest api</Link>) from an <Link href="https://redux-toolkit.js.org/api/createAsyncThunk" target="_blank">asyncThunk</Link> function call; once retrieved, state is sets with that value.</p>
             <div className="flex flex-col items-start mt-5">
-                <h3 className="text-2xl">Note</h3>
-                <span>Since <Link className="text-cyan-800 dark:text-cyan-400 hover:underline" href="https://nextjs.org/blog/next-13">NextJS 13</Link>, all components are <Link className="text-cyan-800 dark:text-cyan-400 hover:underline" href='https://nextjs.org/blog/next-13#server-components'>Server Components</Link>.<br />You must specify <Link className="text-cyan-800 dark:text-cyan-400 hover:underline" href='https://nextjs.org/docs/app/api-reference/directives/use-client'>'use client'</Link> in order to work with <Link className="text-cyan-800 dark:text-cyan-400 hover:underline" href='https://react.dev/reference/react/hooks'>React hooks</Link>.</span>
+                <h3 className="text-xl">What I've learned:</h3>
+                <ul className="list-disc list-inside">
+                    <li><Link href="https://redux-toolkit.js.org/" target="_blank">Redux toolkit</Link></li>
+                    <li><Link href="https://nextjs.org/docs/app/getting-started/route-handlers" target="_blank">Route handlers</Link></li>
+                </ul>
             </div>
+            <hr className="mt-5 mb-5" />
+            <Counter initialValue={1} />
         </>
     );
 }

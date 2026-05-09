ea30774ce9c1258f2cbea2e9dffc1e304d51930c
Javier Gimenez <jsgimenez@gmail.com>
2026-05-09 17:23:59 -0300

    chore(counterApp): removes unnecessary imports and functions definitions. also, includes asyncthunk function call in order to get initial state value

--- app/(dashboard)/counter/counterApp.tsx
+++ app/(dashboard)/counter/counterApp.tsx
@@ -1,36 +1,30 @@
 'use client';
 
 import { Button } from "@/components/ui/button"
-import { useAppSelector } from '@/store';
-import { useDispatch } from 'react-redux';
-import { addOne, substractOne, resetCount } from '@/store/counter/counterSlice';
-import { CounterResponse } from '@/app/api/counter/CounterResponse';
+import { addOne, substractOne, resetCount, fetchInitialData } from '@/store/counter/counterSlice';
+import { useAppDispatch, useAppSelector } from "@/store";
+import { useEffect } from "react";
 
 interface Props {
     initialValue?: number
 }
 
-const getApiCounter = async (): Promise<CounterResponse> => {
-    const data = await fetch('/api/counter').then(response => response.json())
-    return data;
-}
-
 export const Counter = ({ initialValue = 0 }: Props) => {
-    const count = useAppSelector((state) => state.counter.count);
-    const dispatch = useDispatch();
 
-    //const [value, setvalue] = useState(initialValue);
+    const { isReady, count } = useAppSelector((state) => state.counter);
+    const dispatch = useAppDispatch();
+
+    useEffect(() => {
+        dispatch(fetchInitialData())
+    }, [dispatch])
+
     const handleAddClick = () => {
-        //setvalue(value + 1)
         dispatch(addOne())
     }
     const handleResetClick = () => {
-        //setvalue(0)
         dispatch(resetCount(0))
     }
     const handleSubstractClick = () => {
-        //if (value === 0) return
-        //setvalue(value - 1)
         dispatch(substractOne())
     }
 
@@ -38,12 +32,12 @@ export const Counter = ({ initialValue = 0 }: Props) => {
         <div className='flex flex-col mt-10 mb-10'>
 
             <h2 className="text-3xl font-semibold tracking-tight">
-                Current value: {count}
+                {!isReady ? <>retrieving value from /api/counter</> : `Current value: ${count}`}
             </h2>
             <div className='flex gap-3 mt-5'>
-                <Button onClick={handleSubstractClick}>-1</Button>
-                <Button onClick={handleResetClick}>reset to zero</Button>
-                <Button onClick={handleAddClick}>+1</Button>
+                <Button disabled={!isReady} onClick={handleSubstractClick}>-1</Button>
+                <Button disabled={!isReady} onClick={handleResetClick}>reset to zero</Button>
+                <Button disabled={!isReady} onClick={handleAddClick}>+1</Button>
             </div>
         </div>
     )
